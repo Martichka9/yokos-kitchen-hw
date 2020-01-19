e328a6252baf513f533204bb4e0780162d441ef9
Martichka9 <Martichka9@users.noreply.github.com>
2020-01-19 19:23:09 +0200

    style fixes, menu icon, responsive menu

--- docs/assets/js/slick-style.js
+++ docs/assets/js/slick-style.js
@@ -6,24 +6,5 @@ $('.popular-dishes').slick({
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+    variableWidth: true
   });
